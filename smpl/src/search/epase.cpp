e852eea447441db56367070d81a79000e78b6daf
Shohin Mukherjee <shohin.93@gmail.com>
2022-06-21 11:45:06 -0400

    Printing state and edge expansion times

--- smpl/src/search/epase.cpp
+++ smpl/src/search/epase.cpp
@@ -226,6 +226,15 @@ int EPASE::replan(
     // }
 
     extractPath(goal_state, *solution, *cost);
+    
+    cout << "*********************" << endl;
+    cout << "Planning time: " << to_seconds(m_search_time) << endl;;
+    cout << "Num state expansions: " << m_num_state_expansions << endl;
+    cout << "Num edge evals: " << m_num_edge_evals << endl;
+    cout << "State expansions/second: " << m_num_state_expansions/to_seconds(m_search_time) << endl;
+    cout << "Edge evaluations/second: " << m_num_edge_evals/to_seconds(m_search_time) << endl;    
+    cout << "*********************" << endl;
+
     return !SUCCESS;
 }
 
@@ -507,6 +516,7 @@ void EPASE::initialize()
     m_terminate = false;
     m_recheck_flag = true;
     m_num_state_expansions = 0;
+    m_num_edge_evals = 0;
     
     m_edge_expansion_vec.clear();
     m_edge_expansion_vec.resize(m_num_threads, NULL);
@@ -923,8 +933,8 @@ void EPASE::expandEdge(EdgePtrType& edge_ptr, int thread_id)
         edge_ptr->parent_state_ptr->num_successors = cheap_succs.size() + expensive_succs.size();
 
         expandEdgesReal(edge_ptr, cheap_succs, thread_id);
-        
         edge_ptr->parent_state_ptr->num_expanded_successors += cheap_succs.size();
+        m_num_edge_evals+=cheap_succs.size();
 
         // for (auto sidx: cheap_succs)
         // {
@@ -965,6 +975,8 @@ void EPASE::expandEdge(EdgePtrType& edge_ptr, int thread_id)
         // Real edge, evaluate and add proxy edges for child 
         expandEdgeReal(edge_ptr, thread_id);
         edge_ptr->parent_state_ptr->num_expanded_successors += 1;
+        m_num_edge_evals+=1;
+
     }
   
 
